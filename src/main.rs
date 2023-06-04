f7007d562f360a94497e24a760bf98d00b1852c4
Caio Gomes <ocaioluis@gmail.com>
2023-06-04 14:13:48 +0200

    Add dependecies and start up web server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, post, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Salve chat")
+}
+
+#[post("/echo")]
+async fn echo<'a>(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .service(echo)
+    })
+    .bind(("127.0.0.1", 8000))?
+    .run()
+    .await
 }
